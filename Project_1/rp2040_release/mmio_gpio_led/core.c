f55f159bb8fc303aff63a9250912c6b53ddc38b6
Mike Smith <mikesmith9107@gmail.com>
2025-03-17 18:41:55 -0400

    Fixed name errors in reset

--- Project_1/rp2040_release/mmio_gpio_led/core.c
+++ Project_1/rp2040_release/mmio_gpio_led/core.c
@@ -1,18 +1,103 @@
-#include <ioregs.h>
 #include <bitmanip.h>
+
+#include <clocks.h>
+
+#include <ioregs.h>
+
 #include <resets.h>
 
-int main(void) {
-	/* we first must take out IO_BANK0 out of reset mode */
-	RESETS.reset &= ~(1u << RESETS_reset_io_bank0);	/* deassert reset bit */
-	/* wait until the reset is done */
-	loop_until_bit_is_set(RESETS.reset_done, RESETS_reset_done_io_bank0);
-	
-	/* set GPIO25 (LED_PIN) to use the SIO function */
-	IO_BANK0.io[LED_PIN].ctrl = 5;			/* SIO driver for IO[LED_PIN] */
 
-	/* and now we can set the pin to output */
-	SIO.gpio_oe_set = (1u << LED_PIN);		/* set the pin as output */
-	SIO.gpio_out_set = (1u << LED_PIN);		/* and set it high */
+
+/* the following assumes the extra credit from Homeworks 1 and 2 were done; if
+
+ * not, then uncomment the function call in main() */
+
+/* use the first user constructor to set up resets */
+
+__attribute__((constructor(101)))
+
+void do_unresets(void) {
+
+	/* lift IO_BANK0 out of reset */
+
+	RESETS.reset &= ~(1u << RESETS_reset_io_bank0);
+
+	loop_until_bit_is_set(RESETS.reset_done, RESETS_reset_io_bank0);
+
+
+
+	/* lift system PLL out of reset */
+
+	RESETS.reset &= ~(1u << RESETS_pll_sys);
+
+	loop_until_bit_is_set(RESETS.reset_done, RESETS_pll_sys);
+
+}
+
+
+
+int main(void) {
+
+	/* uncomment the following if extra credit for the first two homeworks was
+
+	 * not done */
+
+	do_unresets();
+
+
+
+	/* enable the oscillator */
+
+	XOSC.ctrl = XOSC_enable | XOSC_freq_range_1_15MHz;
+
+	loop_until_bit_is_set(XOSC.status, XOSC_stable);
+
+
+
+	/* enable the system PLL (150 MHz) */
+
+	PLL_SYS.cs = 1;		/* program the reference clock divider */
+
+	PLL_SYS.fbdiv_int = 125;	/* then the feedback divider */
+
+	PLL_SYS.pwr &= ~(
+
+				(1u << PLL_pwr_pd)		/* turn on main power */
+
+			|	(1u << PLL_pwr_vcopd));	/* and VCO */
+
+	loop_until_bit_is_set(PLL_SYS.cs, PLL_lock);	/* wait for VCO lock */
+
+
+
+
+
+	PLL_SYS.prim =						/* set up postdividers */
+
+					(5 << PLL_postdiv1_offset)
+
+				|	(2 << PLL_postdiv2_offset);
+
+	PLL_SYS.pwr &= ~(1u << PLL_pwr_postdivpd);	/* turn the postdividers */
+
+
+
+	/* enable CLK_GPOUT0 and set it to the system PLL */
+
+	CLOCKS.clk_gpout0_ctrl =
+
+					(1u << CLOCKS_enable)
+
+				|	(CLOCKS_GPOUT_clksrc_pll_sys);
+
+
+
+	/* and set GP21 to export that clock */
+
+	IO_BANK0.io[21].ctrl = 8;
+
+
+
 	return 0;
+
 }
