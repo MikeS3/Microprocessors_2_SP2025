41a1ba4ca8c2972b518f440f76bb97485eb92e00
Mike Smith <mikesmith9107@gmail.com>
2025-05-03 13:21:38 -0400

    Project 3 updated from project 1

--- Project_3/rp2040_release/mmio_gpio_led/core.c
+++ Project_3/rp2040_release/mmio_gpio_led/core.c
@@ -9,8 +9,17 @@
 #include <uart-baud.h>
 #include "llinit.h"
 
-#define MAX7219_CMD(a, b)  (((a) << 8u) | (b))
-
+#if 1
+static inline void delay(unsigned n) {
+	asm volatile (
+		"1:	sub %[r0], #1"	"\n"
+		"	bne 1b"
+		: [r0]"+r"(n)
+		:
+		: "memory"
+	);
+}
+#endif
 int main(void) {
 
 	low_level_init();
@@ -19,13 +28,44 @@ int main(void) {
 	spi0_init();
 
 
-	//enable systick interrupt:
-	SYSTICK.csr = (1 << TICKINT_OFFSET) | (1 << CLKSOURCE_OFFSET);
-	//set SYSTICK upper bound to 1_200_000 for 100ms on 12MHz clock
-	SYSTICK.rvr = 1200000;
+	//set SYSTICK upper bound to 12_500_000 for 100ms on 125MHz clock
+	SYSTICK.rvr = 12500000;
+
+	SYSTICK.cvr = 0;
+
+	//enable processor clock as systick clock and interrupts when systick reaches 0
+	SYSTICK.csr = (TICKINT_OFFSET) | (CLKSOURCE_OFFSET);
+
 	//enable systick
 	SYSTICK.csr |= 1;
 	
+
+	#define MAX7219_CMD(a, b)  (((a) << 8u) | (b))
+	const static unsigned max7219_init[] = {
+        MAX7219_CMD(0xc, 0),
+        MAX7219_CMD(0xf, 0),
+        MAX7219_CMD(0xb, 7),
+        MAX7219_CMD(0x9, 0),
+        0
+    };
+
+	for(const unsigned* p = max7219_init; *p; p++) {
+		spi0_send(*p);
+	};
+	
+	for(unsigned i = 1; i < 9; i++) {
+		spi0_send(MAX7219_CMD(i, 0));
+	};
+	
+	spi0_send(MAX7219_CMD(0xc, 1));
+#if 0
+	unsigned char val = 0;
+	while(1) {
+		spi0_send(MAX7219_CMD(0x1, val));
+		val++;
+		delay(1000000);
+	}
+#endif
 	return 0;
 
 }
--- Project_3/rp2040_release/mmio_gpio_led/spi0.c
+++ Project_3/rp2040_release/mmio_gpio_led/spi0.c
@@ -50,36 +50,14 @@ ISR(SPI0_vect) {
 	column_data = img[current_image].data[local_index];
 	
 	//MAX7219_CMD(0x1, column_data);
-	spi0_send(((0x1) << 8u) | (column_data));
-	NVIC_ICPR = 1 << NVIC_BIT(SPI0_vect); //clear interrupt pending register
+	spi0_send(((total_sent) << 8u) | (column_data));
+	//NVIC_ICPR = 1 << NVIC_BIT(SPI0_vect); //clear interrupt pending register
 	}
 
 	//after 8 sent, clear ISER
-	NVIC_ICER = 1 << NVIC_BIT(SPI0_vect); //clear interrupt pending register
+	//NVIC_ICER = 1 << NVIC_BIT(SPI0_vect); //clear interrupt pending register
 
 	//ISCR clear
 	//after 8 sent, clear ICPR
 }
 
-/*
-#define MAX7219_CMD(a, b)  (((a) << 8u) | (b))
-const static unsigned max7219_init[] = {
-        MAX7219_CMD(0xc, 0),
-        MAX7219_CMD(0xf, 0),
-        MAX7219_CMD(0xb, 7),
-        MAX7219_CMD(0x9, 0),
-        0
-    };
-
-/* initialize SPI somewhere, then assuming a function with prototype */
-/* void spi_send(unsigned) 
-for(const unsigned* p = max7219_init, *p, p++) {
-    spi_send(*p);
-}
-
-for(unsigned i = 1; i < 9; i++) {
-    spi_send(MAX7219_CMD(i, 0));
-}
-
-spi_send(MAX7219_CMD(0xc, 1));
-*/
