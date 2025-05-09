7a46eb1751ce490bf41e98d6b049eb1ed14b34d3
crocha <chrisrocha3317@gmail.com>
2025-05-09 01:56:29 -0400

    finish 4

--- project_4/core/periph/mpu.c
+++ project_4/core/periph/mpu.c
@@ -5,38 +5,59 @@
 #include <sysctrl.h>
 #include <stacks.h>
 #include <task.h>
+extern unsigned int __ro_section_start[] __attribute__((weak)); //read only start
+extern unsigned int __ro_section_end[] __attribute__((weak)); //ro end
+extern unsigned int __data_start[] __attribute__((weak));//data start
+extern unsigned int __periph_base[] __attribute__((weak)); //periph base
 
 void mpu_setup_user(struct task_struct* proc) {
 #if 1
-#	warning "Graduate students must implement this function"
-#	warning "otherwise change the 1 above to a 0"
+//#	warning "Graduate students must implement this function"
+//#	warning "otherwise change the 1 above to a 0"
 #endif
-
-	/* disable the MPU */
+	MPU.ctrl = 0; /* disable the MPU */
 	/* set the region for the task being scheduled */
+	mpu_set_region(0, proc->text_start, MPU_ATTRIB_UCODE);
+
+	mpu_set_region(1, proc->data_start, MPU_ATTRIB_UDATA);
+
 	/* start with .text then .data */
 	/* enable the MPU again */
+	 MPU.ctrl = (1u << 0);
 }
 
 void mpu_setup_supervisor(void) {
 	/* supervisor has access to all of flash */
-#if 1
+#if 0
 #	warning "Graduate students must implement this function"
 #	warning "otherwise change the 1 above to a 0"
 #endif
+	MPU.ctrl = 0; /* disable the MPU */
 	/* set the region for the .text section of the supervisor to be all of the
 	 * supervisor flash */
+	
+
+	mpu_set_region(0, __ro_section_start, MPU_ATTRIB_SCODE);
+
 	/* the supervisor should have access to all of SRAM */
+	mpu_set_region(1, __data_start, MPU_ATTRIB_SDATA);
+
 	/* yes, technically we could do some SMEP/SMAP thing here, but that is
 	 * overkill for this assignment :) */
+	MPU.ctrl = (1u << 0);
+
 }
 
 void mpu_setup_periph(void) {
-#if 1
+#if 0
 #	warning "Graduate students must implement this function"
 #	warning "otherwise change the 1 above to a 0"
 #endif
-	/* all peripheral space should be only accessible to the supervisor */
+	MPU.ctrl = 0; /* disable the MPU */
+
+    mpu_set_region(2, &__periph_base, ATTRIB_PERIPH_AREA2);
+	MPU.ctrl = (1u << 0);
+/* all peripheral space should be only accessible to the supervisor */
 }
 
 
--- project_4/include/core/dma.h
+++ project_4/include/core/dma.h
@@ -28,5 +28,23 @@ extern struct {
 	volatile unsigned chan_abort;
 	volatile unsigned n_channels;
 } DMA;
+#define DMA_CTRL_DREQ_PIO0_TX0	(0u)
+
+#define DMA_CTRL_TRIG_TREQ_SEL_OFFSET	(15)
+#define DMA_CTRL_TRIG_TREQ_SEL_MASK		(0x3fu)
+#define DMA_CTRL_RING_SEL			(10)
+#define DMA_CTRL_RING_SIZE_OFFSET	(6)
+#define DMA_CTRL_RING_SIZE_MASK		(0xfu)
+#define DMA_CTRL_INCR_READ			(4)
+#define DMA_CTRL_DATA_SIZE_OFFSET	(2)
+#define DMA_CTRL_DATA_SIZE_MASK		(0x3u)
+#define DMA_CTRL_ENABLE				(0)
+
+#define DMA_CTRL_DATA_SIZE_WORD		(2u)
+#define DMA_CTRL_DATA_SIZE_HALFWORD	(1u)
+#define DMA_CTRL_DATA_SIZE_BYTE		(0u)
+
+#define DMA_CTRL_RING_SEL_READ		(0u)
+#define DMA_CTRL_RING_SEL_WRITE		(1u)
 
 #endif	/* DMA_H__ */
