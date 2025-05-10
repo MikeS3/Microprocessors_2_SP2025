05eaf33e10a8c165e8219aabbad54bcb98d2c4bb
Mike Smith <mikesmith9107@gmail.com>
2025-05-09 21:19:46 -0400

    readded pid controller stuff

--- Project_3/rp2040_release/mmio_gpio_led/pid.h
+++ Project_3/rp2040_release/mmio_gpio_led/pid.h
@@ -1,4 +1,41 @@
-#ifndef RTC_H__
-#define RTC_H__
+#ifndef PID_H__
+#define PID_H__
+
+//PID Adapted from https://github.com/pms67/PID/tree/master
+
+typedef struct {
+
+	/* Controller gains */
+	fixedpt Kp;
+	fixedpt Ki;
+	fixedpt Kd;
+
+	/* Derivative low-pass filter time constant */
+	fixedpt tau;
+
+	/* Output limits */
+	fixedpt limMin;
+	fixedpt limMax;
+	
+	/* Integrator limits */
+	fixedpt limMinInt;
+	fixedpt limMaxInt;
+
+	/* Sample time (in seconds) */
+	fixedpt T;
+
+	/* Controller "memory" */
+	fixedpt integrator;
+	fixedpt prevError;			/* Required for integrator */
+	fixedpt differentiator;
+	fixedpt prevMeasurement;		/* Required for differentiator */
+
+	/* Controller output */
+	fixedpt out;
+
+} PIDController;
+
+void  PIDController_Init(PIDController *pid);
+fixedpt PIDController_Update(PIDController *pid, fixedpt setpoint, fixedpt measurement);
 
 #endif
